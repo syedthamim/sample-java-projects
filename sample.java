e65047d8ab115bc19acea449ab09c4d3e804fa4e
rahil <rahil@oracle.com>
2019-06-14 06:22:45 +0000

    code modified by rahil - development team

--- sample.java
+++ sample.java
@@ -3,6 +3,8 @@ class sample
 
 	public static void main(String[] args){
 		System.out.println("java code example");
-		System.out.println("Modified by - Rahil");
+		System.out.println("Modified by - Rahil - samsung ");
+		System.out.println("code revised in development branch");
+
 	}	
 }
