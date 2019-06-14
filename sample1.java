5159b52465578740017b0e270039610ce7bdcd50
rahil <rahil@oracle.com>
2019-06-14 07:04:50 +0000

    some some modifications

--- sample1.java
+++ sample1.java
@@ -2,8 +2,6 @@ class sample
 {
 
 	public static void main(String[] args){
-		System.out.println("java code example");
-		System.out.println("Modified by - Rahil - samsung ");
 		System.out.println("code revised in development branch");
 		System.out.println("QA Team fired ");
 
